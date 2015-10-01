7e0b8e8803b65075ee8bd751f5d8df3074ff3e4a
Fabio Gianini <fabio.gianini@ibrows.ch>
2015-10-01 13:06:09 +0200

    add start function

--- service.js
+++ service.js
@@ -41,12 +41,14 @@ app.all("/img/*/*", function (req, res) {
 
 });
 
+function start (){
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
+}
 
 
 function getRandomImage(){
