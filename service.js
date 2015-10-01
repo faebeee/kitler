a7b38ec9112ccebf122395d48ee4536cacd6b298
Fabio Gianini <fabio.gianini@ibrows.ch>
2015-10-01 08:39:57 +0200

    add package.json

--- service.js
+++ service.js
@@ -1,35 +1,44 @@
-var imageBaseUrl = "http://www.catsthatlooklikehitler.com/kitler/pics/kitler";
+var config = {
+    "url" : "http://www.catsthatlooklikehitler.com/kitler/pics/kitler"
+};
 
 
 var http = require('http');
 var request = require('request');
 var express = require('express');
 var fs = require('fs');
-var gm = require('gm');
-var uuid = require('node-uuid');
+var lwip = require('lwip');
 
 var app = express();
-var downloadFolder = "./download/";
 
 app.all("/img/*/*", function (req, res) {
     var width = req.params[0];
     var height = req.params[1];
-    var image = downloadFolder+uuid.v4()+".jpg";
     var randImg = getRandomImage();
-    // var writeStream = fs.createWriteStream(image);
-    // var readStream = fs.createReadStream(image);
 
-    request.get(randImg, function (err, _res, body) {
-       var buffer = new Buffer(body, 'binary');
-       var rs = gm(buffer).resize(240, 240).stream(function (err, stdout, stderr) {
-          var writeStream = fs.createWriteStream('path/to/my/resized.jpg');
-          stdout.pipe(writeStream);
+    request({url:randImg,  encoding: 'binary'}, function onImageResponse(err, imageResponse, imageBody) {
+        if (err) throw err;
+
+        var buffer = new Buffer(imageBody, "binary");
+
+        res.writeHead(200, {'Content-Type': 'image/jpg' });
+        res.end(buffer, 'binary');
+        return;
+        // TODO resizing stuff
+        lwip.open(buffer, 'jpg', function(err, image){
+            if (err) throw err;
+
+            lwip.create(width, height, "yellow", function(err, image){
+                if (err) throw err;
+            });
+            /*
+            image.resize(width, height, 'lanczos', function(err, image){
+                if (err) throw err;
+            });
+            */
         });
-
-    //.toBuffer('PNG',function (err, buffer) {
-    //        console.log(buffer);
-    //    });
     });
+
 });
 
 var server = app.listen(3000, function () {
@@ -41,21 +50,6 @@ var server = app.listen(3000, function () {
 
 
 function getRandomImage(){
-    var _url = imageBaseUrl+ Math.round(Math.random() * (8000 - 0 + 1))+".jpg";
+    var _url = config.url+ Math.round(Math.random() * (8000 - 0 + 1))+".jpg";
     return _url;
 }
-
-var download = function(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    if (err) callback(err, filename);
-    else {
-        var stream = request(uri);
-        stream.pipe(
-            fs.createWriteStream(filename)
-            )
-        .on('close', function() {
-            callback(null, filename);
-        });
-    }
-  });
-};
