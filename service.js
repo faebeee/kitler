d22ec2cb46f0b5b0bde3d9e3e480b0d51c1f98a2
Fabio Gianini <fabio.gianini@ibrows.ch>
2015-10-02 13:32:56 +0200

    add static content

--- service.js
+++ service.js
@@ -13,8 +13,17 @@ var fs = require('fs');
 var lwip = require('lwip');
 var Flickr = require("node-flickr");
 var keys = {"api_key": config.flickr.api};
-flickr = new Flickr(keys);
+var flickr = new Flickr(keys);
 var maxPageSearch = 15;
+var app = express();
+
+var server = app.listen(config.port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
 
 function getImage(width, height, callback, page){
     page = page || 1;
@@ -35,7 +44,7 @@ function getImage(width, height, callback, page){
                     return image.source;
                 }
             }
-            console.log('no image found. looking on page '+page);
+            console.warn('no image found. looking on page '+page);
             if(page >= maxPageSearch){
                 throw ("No Image found in "+maxPageSearch+" Pages");
             }
@@ -44,38 +53,59 @@ function getImage(width, height, callback, page){
     });
 }
 
-var app = express();
+function sendBufferedImage( img, res ){
+    img.toBuffer('jpg', function(err, buff){
+        if (err) throw err;
+
+        res.writeHead(200, {'Content-Type': 'image/jpg' });
+        res.end(buff, 'binary');
+    });
+}
+
+function proceedImage(source, width, height, res){
+    width = width || null;
+    height = height || null;
+    request({url:source,  encoding: 'binary'}, function onImageResponse(err, imageResponse, imageBody) {
+        if (err) throw err;
+
+        var buffer = new Buffer(imageBody, "binary");
+        lwip.open(buffer, 'jpg', function(err, img){
+            if (err) throw err;
+            if(width && height){
+                img.crop(width, height, function(err, img){
+                    if (err) throw err;
+                    sendBufferedImage(img, res);
+                });
+            }else{
+                sendBufferedImage(img, res);
+            }
+        });
+    });
+}
 
 app.all("/img/*/*", function (req, res) {
     var width = parseInt(req.params[0]);
     var height = parseInt(req.params[1]);
 
     getImage(width, height, function( uri ){
-        request({url:uri,  encoding: 'binary'}, function onImageResponse(err, imageResponse, imageBody) {
-            if (err) throw err;
-
-            var buffer = new Buffer(imageBody, "binary");
-            lwip.open(buffer, 'jpg', function(err, img){
-                if (err) throw err;
-
-                img.crop(width, height, function(err, img){
-                    if (err) throw err;
-
-                    img.toBuffer('jpg', function(err, buff){
-                        if (err) throw err;
-
-                        res.writeHead(200, {'Content-Type': 'image/jpg' });
-                        res.end(buff, 'binary');
-                    });
-                });
-            });
-        });
+        proceedImage(uri, width, height, res);
     });
 });
 
-var server = app.listen(config.port, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.all("/my/*/*/*", function (req, res) {
+    var id = parseInt(req.params[0]);
+    var width = parseInt(req.params[1]);
+    var height = parseInt(req.params[2]);
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    flickr.get("photos.getSizes", {"photo_id":id}, function(err, result){
+        if (err) throw err;
+        var images = result.sizes.size, len = images.length;
+        for(var i=0; i < len; i++){
+            var image = images[i];
+            if(image.width >= width && image.height >= height){
+                proceedImage(image.source, width, height, res);
+                return;
+            }
+        }
+    });
 });
