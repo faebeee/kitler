071e875a22920449c30a69cc207f971bf8526b30
Fabio Gianini <fabio.gianini@ibrows.ch>
2015-10-02 10:55:10 +0200

    implemented flickr

--- service.js
+++ service.js
@@ -1,42 +1,70 @@
 var config = {
     "url" : "http://www.catsthatlooklikehitler.com/kitler/pics/kitler",
-    'port' : process.env.PORT||3000
+    'port' : process.env.PORT||3000,
+    "flickr" : {
+        "api" : "fc9228d0f911430403a814d59edaf13b",
+        "secret" : "085b8c8b641fc96c"
+    }
 };
-
-var flickr = new Flickr({
-  api_key: "fc9228d0f911430403a814d59edaf13b"
-});
 
 var http = require('http');
 var request = require('request');
 var express = require('express');
 var fs = require('fs');
 var lwip = require('lwip');
+var Flickr = require("node-flickr");
+var keys = {"api_key": config.flickr.api};
+flickr = new Flickr(keys);
+
+function getImage(width, height, callback){
+    flickr.get("photos.search", {"text":"kitler, cat"}, function(err, result){
+        if (err) return console.error(err);
+        var max = result.photos.photo.length;
+        var randId = Math.round( Math.random() * max );
+        var randImg = result.photos.photo[randId];
+
+        flickr.get("photos.getSizes", {"photo_id":randImg.id}, function(err, result){
+            if (err) return console.error(err);
+
+            var images = result.sizes.size, len = images.length;
+            for(var i=0; i < len; i++){
+                var image = images[i];
+                if(image.width >= width && image.height >= height){
+                    callback(image.source);
+                    return image.source;
+                }
+            }
+            throw "no image found";
+        });
+    });
+}
 
 var app = express();
 
 app.all("/img/*/*", function (req, res) {
     var width = parseInt(req.params[0]);
     var height = parseInt(req.params[1]);
-    var randImg = getRandomImage();
 
-    request({url:randImg,  encoding: 'binary'}, function onImageResponse(err, imageResponse, imageBody) {
-        if (err) throw err;
-
-        var buffer = new Buffer(imageBody, "binary");
-        lwip.open(buffer, 'jpg', function(err, img){
+    getImage(width, height, function( uri ){
+        request({url:uri,  encoding: 'binary'}, function onImageResponse(err, imageResponse, imageBody) {
             if (err) throw err;
 
-            img.resize(width, height, 'lanczos', function(err, img){
+            var buffer = new Buffer(imageBody, "binary");
+            lwip.open(buffer, 'jpg', function(err, img){
                 if (err) throw err;
 
-                img.toBuffer('jpg', function(err, buff){
+                img.crop(width, height, function(err, img){
                     if (err) throw err;
 
-                    res.writeHead(200, {'Content-Type': 'image/jpg' });
-                    res.end(buff, 'binary');
+                    img.toBuffer('jpg', function(err, buff){
+                        if (err) throw err;
+
+                        res.writeHead(200, {'Content-Type': 'image/jpg' });
+                        res.end(buff, 'binary');
+                    });
                 });
             });
+
         });
 
     });
@@ -49,9 +77,3 @@ var server = app.listen(config.port, function () {
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
-
-function getRandomImage(){
-    var _url = config.url+ Math.round(Math.random() * (8000 - 0 + 1))+".jpg";
-    return _url;
-}
